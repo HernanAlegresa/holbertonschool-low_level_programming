86f65fbb0f47705377fa7606b9320623507c3abd
root <alegresa@gmail.com>
2023-10-11 06:13:30 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main, serves the entry point for the program.
+ * main, serves the entry point for the program: "Programming is like building a multilingual puzzle"
  *
- * return, indicates that the program has executed without errors.
+ * return 0, indicates that the program has executed without errors.
  */
-int main(void) {
-    puts("Programming is like building a multilingual puzzle\n");
-    return 0;
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
 }
 
