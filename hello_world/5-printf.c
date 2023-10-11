8ef9295490ea7d7762e099dda6e18859d801bda4
root <alegresa@gmail.com>
2023-10-11 07:05:22 -0700

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main-prints "Programming is like building a, followed by a new line.
+*
+* Return: always 0.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
