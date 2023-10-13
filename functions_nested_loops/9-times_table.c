864b93e845fb6f02ab353cdb15212814b22f9b41
root <alegresa@gmail.com>
2023-10-13 09:58:39 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,20 +8,20 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			product = i * j;
+		_putchar ('0');
 
-			if (j == 0)
-			{
-				_putchar(',');
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar('');
+
+			product = i * j;
+			
+			if (product < 10)
 				_putchar(' ');
-				if (product < 10)
-				{
-					_putchar(' ');
-				}
-			}
-			_putchar((product / 10) + '0');
+			else
+				_putchar((product / 10) + '0');
+
 			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
