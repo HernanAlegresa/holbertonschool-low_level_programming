a5b5fc1cac74165481c1d4bdf594f3835039ec77
root <alegresa@gmail.com>
2023-10-13 07:44:14 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -24,7 +24,8 @@ void times_table(void)
 				{
 					_putchar(' ');
 				}
-				_putchar('0' + product);
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
 			}
 		}
 		_putchar('\n');
