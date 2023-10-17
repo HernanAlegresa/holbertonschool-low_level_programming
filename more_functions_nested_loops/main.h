a34bda9c7312b4aabee8e39d62a5ed5b1b1bb704
root <alegresa@gmail.com>
2023-10-17 06:37:46 -0700

    4

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@ int mul(int a, int b);
 void print_numbers(void);
 void _putchar(char);
 void print_most_numbers(void);
+void _putchar(char);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
