f550ca433f955e4691eb1c559cb77df5c65382b1
root <alegresa@gmail.com>
2023-10-17 07:45:34 -0700

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle using #
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j > 0; j--)
+		{
+			_putchar(' ');
+		}
+
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+
+	_putchar('\n');
+	}
+}
