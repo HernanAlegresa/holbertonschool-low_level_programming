b88b6ed11d41ba0a3fa6a3022786c143bedbd47f
root <alegresa@gmail.com>
2023-10-18 05:42:50 -0700

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_triangle - prints a triangle using #
+ * print_triangle - prints a triangle using
  *
  * @size: size of the triangle
  */
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,17 +1,11 @@
+void _putchar (char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void _putchar(char);
 void print_most_numbers(void);
-void _putchar(char);
 void more_numbers(void);
-void _putchar(char);
 void print_line(int n);
-void _putchar(char);
 void print_diagonal(int n);
-void _putchar(char);
 void print_square(int size);
-void _putchar(char);
 void print_triangle(int size);
-void _putchar(char);
