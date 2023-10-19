8fd9d53275baaddbb00360d04bae8751b6490899
root <alegresa@gmail.com>
2023-10-19 07:29:20 -0700

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * puts2 - Prints every other character of a string, starting with the first character.
+ * puts2 - Prints every other character of a string.
  * @str: Pointer to the string.
  */
 void puts2(char *str)
