db41d4daad5950313ac5e08f6c5367364ec59cd7
root <alegresa@gmail.com>
2023-10-19 08:12:54 -0700

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,12 +5,18 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len;
 
-	while (str[i] != '\0')
+	i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
 	{
 		putchar(str[i]);
-		i += 2;
 	}
 
 	putchar('\n');
