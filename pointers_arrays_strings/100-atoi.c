f1812b915d017ef4529279e9ca3ec546ce73f284
root <alegresa@gmail.com>
2023-10-20 09:36:21 -0700

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,16 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+	int i = 1;
+	unsigned int num = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-			break;
-		i++;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (result * sign);
+	do {
+		if (*s == '-')
+		i *= -1;
+		else if (*s >= '0' && *s <= '9')
+		num = num * 10 + (*s - '0');
+		else if (num > 0)
+		break;
+	} while (*s++);
+	return (num *i);
 }
