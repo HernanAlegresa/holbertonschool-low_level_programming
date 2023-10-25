6c749df291f28741ea05dca7c2b58fb430eac12d
root <alegresah@gmail.com>
2023-10-25 07:55:49 -0700

    5

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * _strpbrk - Searches a string for any of a set of bytes.
  * @s: Pointer to the string to search.
  * @accept: Pointer to the string containing the bytes to search for.
- * Return: Pointer to the first occurrence in s of any byte in accept, or NULL if not found.
+ * Return: Pointer to the first occurrence in s of any byte.
  */
 char *_strpbrk(char *s, char *accept)
 {
