db7cfe66ccb0c089610a6eb1158f1e5c9d98de72
root <alegresah@gmail.com>
2023-10-27 08:32:07 -0700

    6

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the function
+ * @argc: the parameter
+ * @argv: the second parameter
+ *
+ *
+ * Return: prints the number of arguments passed to it
+ */
+int main(int argc, char *argv[])
+{
+
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
