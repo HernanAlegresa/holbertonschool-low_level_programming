a8457c07de31f338a7a4f5937362cfa7fb8f93a2
root <alegresah@gmail.com>
2023-11-05 18:46:07 -0800

    2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the name of the function
+ * @void - the parameter
+ * Return: the file being compiled
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+	return (0);
+}
