b2f924c75425502d747077f6a54a583324609524
root <alegresah@gmail.com>
2023-11-06 12:19:27 -0800

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - the dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: the owner
+ * Return: features of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
