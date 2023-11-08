66027e9890a72e20501a332ffcfd89b728cec7da
root <alegresah@gmail.com>
2023-11-08 07:50:35 -0800

    4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+/**
+ * new_dog - name of the function
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: A function that creates new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	dog_t *newdog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newdog == NULL)
+	{
+		return (NULL);
+	}
+
+	newdog->name = strdup(name);
+	if (newdog->name == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+
+	newdog->owner = strdup(owner);
+
+	if (newdog->owner == NULL)
+	{
+		free(newdog->name);
+		free(newdog);
+		return (NULL);
+
+	}
+
+	newdog->age = age;
+
+	return (newdog);
+}
--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -1,4 +1,4 @@
-julien@ubuntu:~/0x0d. structures, typedef$ cat 2-main.c
+julien@ubuntu:~/0x0d. structures, typedef$ cat 4-main.c
 #include <stdio.h>
 #include "dog.h"
 
@@ -9,11 +9,9 @@ julien@ubuntu:~/0x0d. structures, typedef$ cat 2-main.c
  */
 int main(void)
 {
-    struct dog my_dog;
+    dog_t *my_dog;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
     return (0);
 }
