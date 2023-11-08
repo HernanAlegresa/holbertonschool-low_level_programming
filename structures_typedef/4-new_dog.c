9c1aa197c7079875c8c09903c33ca8623ee01e20
root <alegresah@gmail.com>
2023-11-08 08:22:05 -0800

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,47 +3,36 @@
 #include "dog.h"
 #include <string.h>
 /**
- * new_dog - name of the function
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Return: A function that creates new dog
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: A pointer to the newly created dog, or NULL if it fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
 
-	dog_t *newdog;
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	return (NULL);
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	newdog = malloc(sizeof(dog_t));
-
-	if (newdog == NULL)
+	new_dog->name = _strdup(name);
+	if (new_dog->name == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
 
-	newdog->name = strdup(name);
+	new_dog->age = age;
 
-	if (newdog->name == NULL)
+	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == NULL)
 	{
-		free(newdog);
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
 
-	newdog->owner = strdup(owner);
-
-	if (newdog->owner == NULL)
-	{
-		free(newdog->name);
-		free(newdog);
-		return (NULL);
-
-	}
-
-	newdog->age = age;
-
-	return (newdog);
+	return (new_dog);
 }
