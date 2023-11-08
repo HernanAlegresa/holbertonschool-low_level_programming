ac2393f82568c3583032216cdcd72d50ba8c0c1d
root <alegresah@gmail.com>
2023-11-08 05:28:16 -0800

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * init_dog - the function
- * @d: dog to initialize
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * init_dog - the function name
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ * @d: the letter
+ *
+ *
  * Description: Defines metadata about the dog and type of dog's struct
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
