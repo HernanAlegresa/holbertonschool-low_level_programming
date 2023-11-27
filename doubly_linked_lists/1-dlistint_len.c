42d72829bac9e96a89176212b0789819c4c30ff2
root <alegresah@gmail.com>
2023-11-27 05:01:55 -0800

    1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* dlistint_len - the function.
+* @h: the dlistint_t list.
+* Return: the number of nodes.
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		(c)++;
+	}
+
+	return (c);
+}
