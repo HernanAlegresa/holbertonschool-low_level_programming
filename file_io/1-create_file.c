881e6fb578d0af3dfe65d4c523703e5f54e72d36
root <alegresah@gmail.com>
2023-12-02 15:37:35 -0800

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len] != '\0'; len++);
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
 		bread = write(fd, text_content, len);
 		if (bread == -1 || bread != len)
 		{
