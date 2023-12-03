3c67e66c9fd47ad3dea36d185e9e845418969a54
root <alegresah@gmail.com>
2023-12-02 16:37:34 -0800

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,49 +1,65 @@
 #include "main.h"
 
 /**
-* main - the main function
-* @file_from: name of the source file
-* @file_to: name of the destination file
-* Return: 1 on success, -1 on failure.
+* main - copies the content of a file to another file
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 on success, or an exit code on failure.
 */
 
-int main(const char *file_from, const char *file_to)
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	ssize_t bread, bwrite;
 	char buffer[BUFFER_SIZE];
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		return (-1);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97);
+	}
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
-		return (-1);
+		return (99);
 	}
 
 	while ((bread = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bwrite = write(fd_to, buffer, bread);
+
 		if (bwrite == -1 || bwrite != bread)
 		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			return (-1);
+			return (99);
 		}
 	}
 
 	if (bread == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		return (-1);
+		return (98);
 	}
 
 	if (close(fd_from) == -1 || close(fd_to) == -1)
-		return (-1);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+		return (100);
+	}
 
-	return (1);
+	return (0);
 }
