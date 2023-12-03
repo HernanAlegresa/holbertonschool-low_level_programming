de3c11f703991f0c6221f76261d0145adc63cdd5
root <alegresah@gmail.com>
2023-12-03 08:10:54 -0800

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1 (file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
+	if (file_from == -1 || (file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
 	{
 		if (file_from != -1)
 			close(file_from);
