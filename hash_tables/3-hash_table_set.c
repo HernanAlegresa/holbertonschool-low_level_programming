6f0a2abe7147bf3e4fc5e787174743fbbe75ba63
root <alegresah@gmail.com>
2023-12-03 17:44:01 -0800

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,39 +10,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *current;
-	unsigned long int index;
+	hash_node_t *node;
+	char *cvalue;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	cvalue = strdup(value);
+	if (cvalue == NULL)
+		return (0);
 
-	for (hash_node_t *current = ht->array[index]; current != NULL; current = current->next)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(current->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(current->value);
-			current->value = strdup(value);
-			return (current->value != NULL);
+			free(ht->array[i]->value);
+			ht->array[i]->value = cvalue;
+			return (1);
 		}
 	}
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
-		return (0);
-
-	node->key = strdup(key);
-	node->value = strdup(value);
-
-	if (node->key == NULL || node->value == NULL)
 	{
-		free(node->key);
-		free(node->value);
+		free(cvalue);
+		return (0);
+	}
+	
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
 		free(node);
 		return (0);
 	}
-
+	
+	node->value = cvalue;
 	node->next = ht->array[index];
 	ht->array[index] = node;
 
