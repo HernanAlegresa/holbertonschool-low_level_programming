b061fb6ac74a32375392a590337046616aa365ab
root <alegresah@gmail.com>
2023-12-03 17:04:05 -0800

    0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,11 +8,17 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+	hash_table_t *ht;
 
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
+	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (ht->array == NULL)
@@ -20,11 +26,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
-
-	ht->size = size;
-
-	for (unsigned long int i = 0; i < size; i++)
-	ht->array[i] = NULL;
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}
 
 	return (ht);
 }
