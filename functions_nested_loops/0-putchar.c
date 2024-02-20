a88742b94f63273cb5c9c7312b226d826ede1b90
HernanAlegresa <alegresah@gmail.com>
2024-02-20 14:16:12 +0000

    Updated 0 to 5

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main-prints "Programming is like building a, followed by a new line.
+* Return: always 0.
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabeth from a to z.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+		_putchar(letter);
+		letter++;
+		}
+
+		_putchar('\n');
+	}
+}
