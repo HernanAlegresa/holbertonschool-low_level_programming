d93bfdb8361a3579dfe2bfa6f70fd5dc25816a02
HernanAlegresa <alegresah@gmail.com>
2024-02-26 16:57:07 +0000

    Updated 5 to 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - the function name
+ * @letters: the parameter
+ *
+ *
+ * Return: the value
+ */
+char *leet(char *letters)
+{
+
+char lookup[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+char rep[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+int i;
+int j;
+	for (i = 0; letters[i] != '\0'; i++)
+	{
+
+		for (j = 0; j < 10; ++j)
+
+		{
+			if (letters[i] == lookup[j])
+
+
+			{
+				letters[i] = rep[j];
+
+				break;
+
+			}
+
+		}
+
+	}
+	return (letters);
+}
