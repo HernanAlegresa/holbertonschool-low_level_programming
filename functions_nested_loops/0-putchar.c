e91fadf3096736974eab2a7eec9bf2b08c41eccc
HernanAlegresa <alegresah@gmail.com>
2024-02-27 15:29:31 +0000

    Updated 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-* main - prints _putchar
-* Return: always 0.
-*/
+ * main - print _putchar.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char frase[] = "_putchar";
-	int i;
-	for (i = 0; i <= 7; i++)
-		_putchar(frase[i]);
-		_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
