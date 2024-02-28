4eac78d668e29864540b79f4ba9b67c0a1f4d09e
HernanAlegresa <alegresah@gmail.com>
2024-02-28 14:21:01 +0000

    Updated 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the function
+ * @argc: the first parameter
+ * @argv: the second parameter
+ * Return: Prints all the arguments it recieves
+ */
+int main(int argc, char *argv[])
+{
+
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
