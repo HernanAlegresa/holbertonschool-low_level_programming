585954d19894720cb14488a1ef3194bfe8965b2f
root <alegresah@gmail.com>
2024-03-05 15:43:07 +0000

    Updated 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - the name of the function
+ * struct dog - the characteristics of the dog
+ * @d: the parameter
+ * Return: A function that prints a struct dog
+ */
+void print_dog(struct dog *d)
+{
+
+if (d)
+	{
+	d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+		printf("Age: %f\n", d->age);
+	d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+	}
+
+}
