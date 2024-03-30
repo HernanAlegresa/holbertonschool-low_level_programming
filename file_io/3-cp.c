6e754c4f4973cd5038443278ce2e837ed1e34035
root <alegresah@gmail.com>
2024-03-30 16:24:37 +0000

    Updated 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,1 +1,10 @@
+#include "main.h"
 
+/**
+* main - copies the content of a file to another file
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 on success, or an exit code on failure.
+*/
+
+int main(int argc, char *argv[])
