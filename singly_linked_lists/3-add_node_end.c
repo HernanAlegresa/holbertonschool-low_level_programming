141eb1876c9009c5f9c50690a96bdec86482e231
root <alegresah@gmail.com>
2024-03-30 15:00:40 +0000

    Updated 3 with new node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -25,3 +25,20 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node->len = len;
 	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		node = *head;
+		while (node->next != NULL)
+		{
+			node = last_node->next;
+		}
+		node->next = new_node;
+	}
+
+	return (new_node);
+}
