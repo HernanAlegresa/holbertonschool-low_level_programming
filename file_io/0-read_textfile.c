6a01e1a998cfe152c0f0ef57c5ea4e0c03715126
root <alegresah@gmail.com>
2024-03-30 16:17:18 +0000

    Updated .c, .h, 0 and RDME.md

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* read_textfile - Reads a text file and prints it to the POSIX standard output.
+* @filename: the name of the file
+* @letters: the letters to read and print
+* Return: the letters read and printed
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bread;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bread = read(fd, buffer, letters);
+	if (bread == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	write(STDOUT_FILENO, buffer, bread);
+
+	return (bread);
+}
