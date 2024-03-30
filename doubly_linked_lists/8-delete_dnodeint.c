f1e097ab559202417d4a65560cf29293321d7041
root <alegresah@gmail.com>
2024-03-30 15:50:29 +0000

    Updated 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(node);
+		return (1);
+	}
+
+	for (; index > 0 && node != NULL; index--)
+		node = node->next;
+
+	if (node == NULL)
+		return (-1);
+
+	if (node->prev != NULL)
+		node->prev->next = node->next;
+
+	if (node->next != NULL)
+		node->next->prev = node->prev;
+
+	free(node);
+
+	return (node);
+}
