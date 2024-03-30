d1e96020740e670edca29f8d2ba4eb8e5af1007c
root <alegresah@gmail.com>
2024-03-30 14:57:19 +0000

    Updated 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,6 +4,8 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *node;
 
+	int len;
+
 	if (str == NULL)
 		return (NULL);
 
@@ -17,4 +19,9 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-}
+
+	for (len = 0; str[len] != '\0'; len++)
+	;
+
+	new_node->len = len;
+	new_node->next = NULL;
