b562bcdbd6cc63ff6a0c66d69db7e3dbeee648f5
root <alegresah@gmail.com>
2024-03-30 15:48:02 +0000

    Updated 7 try

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -41,5 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (node1->next != NULL)
 		node1->next->prev = newnode;
 
+	node1->next = newnode;
+
 	return (newnode);
 }
